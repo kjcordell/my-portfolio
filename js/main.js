d7e5d74fbdd4ce014961b259f58a0ca65c6e2ce2
Kevin J Cordell <kjcordell@gmail.com>
2018-06-25 17:37:21 -0400

    updated images

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const myWork = [
         'desc': "Lorem ipsum dolor amet church-key authentic green juice umami chillwave meh pok pok keytar ennui pop-up helvetica organic. Kogi iceland affogato tofu. Actually vexillologist kickstarter succulents disrupt gastropub. Tacos fashion axe selvage microdosing edison bulb. Sartorial quinoa hell of tbh, typewriter health goth celiac tacos yuccie migas adaptogen intelligentsia meggings tote bag.",
         'image': {
             'desc': "example screenshot of a project involving code",
-            'src': "images/example1.png",
+            'src': "images/codeexample.jpeg",
             'comment': ""
         }
     },    
@@ -19,7 +19,7 @@ const myWork = [
         'desc': "Kogi shabby chic chillwave gochujang celiac heirloom. 8-bit art party craft beer, gentrify flannel keytar hella. Marfa coloring book sriracha, master cleanse banh mi freegan four loko. Small batch cronut beard asymmetrical, meggings tbh ethical literally. Bitters plaid fam everyday carry pour-over before they sold out. Street art roof party knausgaard, listicle XOXO viral biodiesel cold-pressed etsy swag everyday carry. Sustainable before they sold out kinfolk, locavore hoodie mixtape semiotics cloud bread tumeric photo booth hella intelligentsia vexillologist try-hard.",
         'image': {
             'desc': "example screenshot of a project involving chemistry",
-            'src': "images/example2.png",
+            'src': "images/Enterprise.jpeg",
             'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
                     https://www.flickr.com/photos/ssoosay/4097410999`
         }
@@ -30,7 +30,7 @@ const myWork = [
         'desc': "Sartorial paleo kitsch bespoke, pok pok cray whatever gentrify stumptown vinyl marfa austin. Tilde brooklyn shabby chic sartorial af enamel pin, asymmetrical pabst bitters master cleanse readymade. Seitan gochujang microdosing swag lo-fi yr street art dreamcatcher la croix everyday carry vegan echo park vaporware. Sartorial biodiesel tattooed actually typewriter cloud bread tofu umami intelligentsia succulents. Biodiesel venmo kogi, hexagon tousled tote bag cronut keffiyeh shoreditch selvage scenester edison bulb ennui vinyl. Small batch tattooed cloud bread, bitters tacos messenger bag plaid la croix microdosing. Neutra godard tote bag cold-pressed, tilde austin iceland.",
         'image': {
             'desc': "example screenshot of a project involving cats",
-            'src': "images/example3.png",
+            'src': "images/Uss_hunley.jpeg",
             'comment': `"Bengal cat” by roberto shabs is licensed under CC BY 2.0
                     https://www.flickr.com/photos/37287295@N00/2540855181`
         }
